191d4bcb0c9d9cebedd1f4e932768568952c5fd2
GlovesforATT <dinhjeffreyy@gmail.com>
2016-03-12 17:04:16 -0800

    added loneTeen.java

---
+++ warmup-1/loneTeen.java
@@ -0,0 +1,12 @@
+public boolean loneTeen(int a, int b) {
+  if ((a>=13 && a<=19) && (b>=13 && b<=19)){
+    return false;
+  }
+  if ((a>=13 && a<=19) && (b<=13 || b>=19)){
+    return true;
+  }
+  if ((a<=13 || a>=19) && (b>=13 && b<=19)){
+    return true;
+  }
+  return false;
+}
