d1193a438cf669ccc4abc6bbe30fe7b44496e56f
dinhjeffrey <dinhjeffreyy@gmail.com>
2016-03-18 12:30:25 -0700

    added middleThree.java

---
+++ string-1/middleThree.java
@@ -0,0 +1,15 @@
+/*
+Given a string of odd length, return the string length 3 from its middle, so "Candy" yields "and". The string length will be at least 3. 
+
+middleThree("Candy") → "and"
+middleThree("and") → "and"
+middleThree("solving") → "lvi"
+*/
+
+public String middleThree(String str) { // pie
+  int middle = (int) str.length()/2;
+  if (str.length() == 3){
+    return str;
+  }
+  return str.substring(middle-1, middle+2);
+}
