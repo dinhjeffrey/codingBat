7e1e8dbfb9d9bd8700b89ed46429d40d3d1df108
dinhjeffrey <dinhjeffreyy@gmail.com>
2016-03-19 19:27:02 -0700

    added frontAgain.java

---
+++ string-1/frontAgain.java
@@ -0,0 +1,9 @@
+public boolean frontAgain(String str) {
+  if (str.length() < 2)
+    return false;
+  if (str.length() == 2)
+    return true;
+  if (str.substring(0,2).equals(str.substring(str.length()-2)) )
+    return true;
+  return false;
+}
