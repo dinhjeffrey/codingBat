6d202734ba8415f21cc1044cbddac1a314c3ab8d
dinhjeffrey <dinhjeffreyy@gmail.com>
2016-03-23 11:58:57 -0700

    Given an array of ints of even length, return a new array length 2 containing the middle two elements from the original array. The original array will be length 2 or more.

---
+++ array-1/makeMiddle.java
@@ -0,0 +1,13 @@
+/* makeMiddle
+Given an array of ints of even length, return a new array length 2 containing the middle two elements from the original array. The original array will be length 2 or more. 
+
+makeMiddle({1, 2, 3, 4}) → {2, 3}
+makeMiddle({7, 1, 2, 3, 4, 9}) → {2, 3}
+makeMiddle({1, 2}) → {1, 2}
+*/
+
+public int[] makeMiddle(int[] nums) {
+  int middle = nums.length / 2;
+  int[] arr = {nums[middle-1], nums[middle]};
+  return arr;
+}
