5e8b7402301cfea8c36fe0d017cd2d7b6403100c
dinhjeffrey <dinhjeffreyy@gmail.com>
2016-04-05 16:15:06 -0700

    add maxMod5

---
+++ logic-1/maxMod5.java
@@ -0,0 +1,20 @@
+/* maxMod5
+Given two int values, return whichever value is larger. However if the two values have the same remainder when divided by 5, then the return the smaller value. However, in all cases, if the two values are the same, return 0. Note: the % "mod" operator computes the remainder, e.g. 7 % 5 is 2. 
+
+maxMod5(2, 3) → 3
+maxMod5(6, 2) → 6
+maxMod5(3, 2) → 3
+*/
+
+public int maxMod5(int a, int b) {
+  if (a%5 == b%5) {
+    if (a<b)
+      return a;
+    if (b<a)
+      return b;
+    return 0;
+  }
+  if (a>b)
+    return a;
+  return b;
+}
