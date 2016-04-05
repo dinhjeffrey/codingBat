f2bfffb4b5acc8194c396f812d4ef0f9f6107c15
dinhjeffrey <dinhjeffreyy@gmail.com>
2016-04-04 19:21:21 -0700

    add teaParty

---
+++ logic-1/nearTen.java
@@ -0,0 +1,11 @@
+/* nearTen
+Given a non-negative number "num", return true if num is within 2 of a multiple of 10. Note: (a % b) is the remainder of dividing a by b, so (7 % 5) is 2. See also: Introduction to Mod 
+
+nearTen(12) → true
+nearTen(17) → false
+nearTen(19) → true
+*/
+
+public boolean nearTen(int num) {
+  return num%10+2==10 || num%10+1==10 || num%10==0 || num%10-1==0 || num%10-2==0 || num==1 || num==2;
+}
