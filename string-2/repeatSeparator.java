df9a16a006d92e4194cd1c8790a25a369c0515d7
dinhjeffrey <dinhjeffreyy@gmail.com>
2016-04-24 09:34:21 -0700

    add repeatSeparator

---
+++ string-2/repeatSeparator.java
@@ -0,0 +1,17 @@
+/* repeatSeparator
+Given two strings, word and a separator sep, return a big string made of count occurrences of the word, separated by the separator string.
+
+repeatSeparator("Word", "X", 3) → "WordXWordXWord"
+repeatSeparator("This", "And", 2) → "ThisAndThis"
+repeatSeparator("This", "And", 1) → "This"
+*/
+
+public String repeatSeparator(String word, String sep, int count) {
+  if (count == 0)
+    return "";
+  String newString = word;
+  for (int i=0; i<count -1; i++) {
+    newString += sep + word;
+  }
+  return newString;
+}
